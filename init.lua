e872e9e386070a8f894e749a85f84a8ce19c71f3
Peter Tajthy <peter.tajthy@genesys.com>
2025-04-08 08:15:40 +0200

    Modular approach and remove orgmode and org-roam, add obsidian

--- init.lua
+++ init.lua
@@ -93,69 +93,7 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
 
--- [[ Setting options ]]
--- See `:help vim.opt`
--- NOTE: You can change these options as you wish!
---  For more options, you can see `:help option-list`
-
--- Make line numbers default
-vim.opt.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
-
--- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = 'a'
-
--- Don't show the mode, since it's already in the status line
-vim.opt.showmode = false
-
--- Sync clipboard between OS and Neovim.
---  Schedule the setting after `UiEnter` because it can increase startup-time.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'
-end)
-
--- Enable break indent
-vim.opt.breakindent = true
-
--- Save undo history
-vim.opt.undofile = true
-
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
--- Keep signcolumn on by default
-vim.opt.signcolumn = 'yes'
-
--- Decrease update time
-vim.opt.updatetime = 250
-
--- Decrease mapped sequence wait time
--- Displays which-key popup sooner
-vim.opt.timeoutlen = 300
-
--- Configure how new splits should be opened
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-
--- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-
--- Preview substitutions live, as you type!
-vim.opt.inccommand = 'split'
-
--- Show which line your cursor is on
-vim.opt.cursorline = true
-
--- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+require 'options'
 
 -- Folding options
 vim.opt.foldcolumn = '0'
@@ -167,6 +105,8 @@ vim.opt.foldnestmax = 3
 vim.opt.foldlevel = 99
 vim.opt.foldlevelstart = 99
 
+vim.opt.conceallevel = 2
+
 local function close_all_folds()
   vim.api.nvim_exec2('%foldc!', { output = false })
 end
@@ -176,55 +116,9 @@ end
 
 vim.keymap.set('n', '<leader>zs', close_all_folds, { desc = '[s]hut all folds' })
 vim.keymap.set('n', '<leader>zo', open_all_folds, { desc = '[o]pen all folds' })
--- set jq for hanndling json
-vim.keymap.set('n', '<leader>jq', ':%!jq "."<CR>', { noremap = true })
 
 -- [[ Basic Keymaps ]]
---  See `:help vim.keymap.set()`
-
--- Clear highlights on search when pressing <Esc> in normal mode
---  See `:help hlsearch`
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-
--- Diagnostic keymaps
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
---
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-
--- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
-
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
---
---  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
--- [[ Basic Autocommands ]]
---  See `:help lua-guide-autocommands`
-
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
+require 'keymaps'
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
@@ -352,173 +246,152 @@ require('lazy').setup({
   -- you do for a plugin at the top level, you can do for a dependency.
   --
   -- Use the `dependencies` key to specify the dependencies of a particular plugin
-
-  { -- Fuzzy Finder (files, lsp, etc)
-    'nvim-telescope/telescope.nvim',
-    event = 'VimEnter',
-    branch = '0.1.x',
+  require 'kickstart.plugins.telescope',
+  require 'kickstart.plugins.neotest',
+  {
+    'mfussenegger/nvim-dap',
     dependencies = {
-      'nvim-lua/plenary.nvim',
-      { -- If encountering errors, see telescope-fzf-native README for installation instructions
-        'nvim-telescope/telescope-fzf-native.nvim',
-
-        -- `build` is used to run some command when the plugin is installed/updated.
-        -- This is only run then, not every time Neovim starts up.
-        build = 'make',
-
-        -- `cond` is a condition used to determine whether this plugin should be
-        -- installed and loaded.
-        cond = function()
-          return vim.fn.executable 'make' == 1
-        end,
-      },
-      { 'nvim-telescope/telescope-ui-select.nvim' },
-
-      -- Useful for getting pretty icons, but requires a Nerd Font.
-      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+      'nvim-neotest/nvim-nio',
+      'rcarriga/nvim-dap-ui',
+      'mfussenegger/nvim-dap-python',
+      'theHamsta/nvim-dap-virtual-text',
     },
     config = function()
-      -- Telescope is a fuzzy finder that comes with a lot of different things that
-      -- it can fuzzy find! It's more than just a "file finder", it can search
-      -- many different aspects of Neovim, your workspace, LSP, and more!
-      --
-      -- The easiest way to use Telescope, is to start by doing something like:
-      --  :Telescope help_tags
-      --
-      -- After running this command, a window will open up and you're able to
-      -- type in the prompt window. You'll see a list of `help_tags` options and
-      -- a corresponding preview of the help.
-      --
-      -- Two important keymaps to use while in Telescope are:
-      --  - Insert mode: <c-/>
-      --  - Normal mode: ?
-      --
-      -- This opens a window that shows you all of the keymaps for the current
-      -- Telescope picker. This is really useful to discover what Telescope can
-      -- do as well as how to actually do it!
+      local dap = require 'dap'
+      local dapui = require 'dapui'
+      local dap_python = require 'dap-python'
 
-      -- [[ Configure Telescope ]]
-      -- See `:help telescope` and `:help telescope.setup()`
-      require('telescope').setup {
-        -- You can put your default mappings / updates / etc. in here
-        --  All the info you're looking for is in `:help telescope.setup()`
-        --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
-        -- pickers = {}
-        extensions = {
-          ['ui-select'] = {
-            require('telescope.themes').get_dropdown(),
-          },
-        },
+      require('dapui').setup {}
+      require('nvim-dap-virtual-text').setup {
+        commented = true, -- Show virtual text alongside comment
       }
 
-      -- Enable Telescope extensions if they are installed
-      pcall(require('telescope').load_extension, 'fzf')
-      pcall(require('telescope').load_extension, 'ui-select')
+      dap_python.setup 'python3'
 
-      -- See `:help telescope.builtin`
-      local builtin = require 'telescope.builtin'
-      vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-      vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-      vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-      vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-      vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-      vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-      vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.fn.sign_define('DapBreakpoint', {
+        text = '',
+        texthl = 'DiagnosticSignError',
+        linehl = '',
+        numhl = '',
+      })
 
-      -- Slightly advanced example of overriding default behavior and theme
-      vim.keymap.set('n', '<leader>/', function()
-        -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-        builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-          winblend = 10,
-          previewer = false,
-        })
-      end, { desc = '[/] Fuzzily search in current buffer' })
+      vim.fn.sign_define('DapBreakpointRejected', {
+        text = '', -- or "❌"
+        texthl = 'DiagnosticSignError',
+        linehl = '',
+        numhl = '',
+      })
 
-      -- It's also possible to pass additional configuration options.
-      --  See `:help telescope.builtin.live_grep()` for information about particular keys
-      vim.keymap.set('n', '<leader>s/', function()
-        builtin.live_grep {
-          grep_open_files = true,
-          prompt_title = 'Live Grep in Open Files',
-        }
-      end, { desc = '[S]earch [/] in Open Files' })
+      vim.fn.sign_define('DapStopped', {
+        text = '', -- or "→"
+        texthl = 'DiagnosticSignWarn',
+        linehl = 'Visual',
+        numhl = 'DiagnosticSignWarn',
+      })
 
-      -- Shortcut for searching your Neovim configuration files
-      vim.keymap.set('n', '<leader>sn', function()
-        builtin.find_files { cwd = vim.fn.stdpath 'config' }
-      end, { desc = '[S]earch [N]eovim files' })
+      -- Automatically open/close DAP UI
+      dap.listeners.after.event_initialized['dapui_config'] = function()
+        dapui.open()
+      end
+
+      local opts = { noremap = true, silent = true }
+
+      -- Toggle breakpoint
+      vim.keymap.set('n', '<leader>db', function()
+        dap.toggle_breakpoint()
+      end, opts)
+
+      -- Continue / Start
+      vim.keymap.set('n', '<leader>dc', function()
+        dap.continue()
+      end, opts)
+
+      -- Step Over
+      vim.keymap.set('n', '<leader>do', function()
+        dap.step_over()
+      end, opts)
+
+      -- Step Into
+      vim.keymap.set('n', '<leader>di', function()
+        dap.step_into()
+      end, opts)
+
+      -- Step Out
+      vim.keymap.set('n', '<leader>dO', function()
+        dap.step_out()
+      end, opts)
+      --
+      -- Keymap to terminate debugging
+      vim.keymap.set('n', '<leader>dq', function()
+        require('dap').terminate()
+      end, opts)
+
+      -- Toggle DAP UI
+      vim.keymap.set('n', '<leader>du', function()
+        dapui.toggle()
+      end, opts)
     end,
   },
-  { 'nvim-neotest/nvim-nio' },
   {
-    'nvim-neotest/neotest',
+    'epwalsh/obsidian.nvim',
+    version = '*',
+    lazy = true,
+    ft = 'markdown',
     dependencies = {
       'nvim-lua/plenary.nvim',
-      'antoinemadec/FixCursorHold.nvim',
-      'nvim-treesitter/nvim-treesitter',
-      'nvim-neotest/neotest-python',
     },
-    config = function()
-      local neotest = require 'neotest'
-      neotest.setup {
-        adapters = {
-          require 'neotest-python' {
-            -- Extra arguments for pytest
-            args = { '--verbose' },
-            -- Runner to use. Will use pytest by default
-            runner = 'pytest',
-          },
+    opts = {
+      workspaces = {
+        {
+          name = 'ptajthy',
+          path = '/Users/ptajthy/Dropbox/notes/obsidian-notes/',
         },
-      }
-
-      -- Keymaps
-      vim.keymap.set('n', '<leader>tt', function()
-        neotest.run.run() -- Run nearest test
-      end, { desc = 'Run neares[T] [T]est' })
-      vim.keymap.set('n', '<leader>tf', function()
-        neotest.run.run(vim.fn.expand '%') -- Run current file
-      end, { desc = 'Run curren[T] [F]ile' })
-      vim.keymap.set('n', '<leader>ts', function()
-        neotest.run.run { suite = true } -- Run all test files
-      end, { desc = 'Run all [T]est file[S]' })
-      vim.keymap.set('n', '<leader>to', function()
-        neotest.output.open { enter = true } -- Open test output
-      end, { desc = 'Open [T]est [O]utput' })
-      vim.keymap.set('n', '<leader>tp', function()
-        neotest.output_panel.toggle() -- Toggle output panel
-      end, { desc = '[T]oggle output [P]anel' })
-      vim.keymap.set('n', '<leader>tw', function()
-        neotest.watch.toggle() -- Toggle watching test
-      end, { desc = '[T]oggle [W]atching [P]anel' })
-      vim.keymap.set('n', '<leader>tC', function()
-        neotest.run.run { args = { '--cov' } }
-      end, { desc = '[T]est All with [C]overage' })
-    end,
+      },
+      notes_subdir = 'inbox',
+      new_notes_location = 'notes_subdir',
+      mappings = {
+        -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+        ['gf'] = {
+          action = function()
+            return require('obsidian').util.gf_passthrough()
+          end,
+          opts = { noremap = false, expr = true, buffer = true },
+        },
+      },
+      completion = {
+        nvim_cmp = true,
+        min_chars = 2,
+      },
+      ui = {
+        -- Disable some things below here because I set these manually for all Markdown files using treesitter
+        checkboxes = {},
+        bullets = {},
+      },
+      templates = {
+        folder = 'templates',
+        date_format = '%Y-%m-%d',
+        time_format = '%H:%M',
+        -- A map for custom variables, the key should be the variable and the value a function
+        substitutions = {},
+      },
+    },
   },
   {
     'kdheepak/lazygit.nvim',
     lazy = true,
     cmd = {
-        "LazyGit",
-        "LazyGitConfig",
-        "LazyGitCurrentFile",
-        "LazyGitFilter",
-        "LazyGitFilterCurrentFile",
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
     },
     dependencies = {
-        "nvim-lua/plenary.nvim",
+      'nvim-lua/plenary.nvim',
     },
     keys = {
-        { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
-    }
+      { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    },
   },
   {
     'ThePrimeagen/refactoring.nvim',
@@ -541,45 +414,11 @@ require('lazy').setup({
     end,
   },
   {
-    'nvim-orgmode/orgmode',
-    event = 'VeryLazy',
-    ft = { 'org' },
-    config = function()
-      -- Setup orgmode
-      require('orgmode').setup {
-        org_agenda_files = '~/Dropbox/notes/nvim/**/*',
-        org_default_notes_file = '~/Dropbox/notes/nvim/refile.org',
-      }
-
-      -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
-      -- add ~org~ to ignore_install
-      -- require('nvim-treesitter.configs').setup({
-      --   ensure_installed = 'all',
-      --   ignore_install = { 'org' },
-      -- })
-    end,
-  },
-  {
     'tadmccorkle/markdown.nvim',
     ft = 'markdown', -- or 'event = "VeryLazy"'
     opts = {
       -- configuration here or empty for defaults
     },
-  },
-  {
-    'chipsenkbeil/org-roam.nvim',
-    tag = '0.1.0',
-    dependencies = {
-      {
-        'nvim-orgmode/orgmode',
-        tag = '0.3.4',
-      },
-    },
-    config = function()
-      require('org-roam').setup {
-        directory = '/Users/ptajthy/OneDrive - Genesys Telecommunications Laboratories, Inc/notes',
-      }
-    end,
   },
   -- LSP Plugins
   {
---
+++ lua/kickstart/plugins/neotest.lua
@@ -0,0 +1,48 @@
+return {
+  { 'nvim-neotest/nvim-nio' },
+  {
+    'nvim-neotest/neotest',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'antoinemadec/FixCursorHold.nvim',
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-neotest/neotest-python',
+    },
+    config = function()
+      local neotest = require 'neotest'
+      neotest.setup {
+        adapters = {
+          require 'neotest-python' {
+            -- Extra arguments for pytest
+            args = { '--verbose' },
+            -- Runner to use. Will use pytest by default
+            runner = 'pytest',
+          },
+        },
+      }
+
+      -- Keymaps
+      vim.keymap.set('n', '<leader>tt', function()
+        neotest.run.run() -- Run nearest test
+      end, { desc = 'Run neares[T] [T]est' })
+      vim.keymap.set('n', '<leader>tf', function()
+        neotest.run.run(vim.fn.expand '%') -- Run current file
+      end, { desc = 'Run curren[T] [F]ile' })
+      vim.keymap.set('n', '<leader>ts', function()
+        neotest.run.run { suite = true } -- Run all test files
+      end, { desc = 'Run all [T]est file[S]' })
+      vim.keymap.set('n', '<leader>to', function()
+        neotest.output.open { enter = true } -- Open test output
+      end, { desc = 'Open [T]est [O]utput' })
+      vim.keymap.set('n', '<leader>tp', function()
+        neotest.output_panel.toggle() -- Toggle output panel
+      end, { desc = '[T]oggle output [P]anel' })
+      vim.keymap.set('n', '<leader>tw', function()
+        neotest.watch.toggle() -- Toggle watching test
+      end, { desc = '[T]oggle [W]atching [P]anel' })
+      vim.keymap.set('n', '<leader>tC', function()
+        neotest.run.run { args = { '--cov' } }
+      end, { desc = '[T]est All with [C]overage' })
+    end,
+  },
+}
